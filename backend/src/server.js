20b1893a1320a87786f0d6b2906b81e09a9d3d7f
Kshitij <kshitij24.srinivasan@gmail.com>
2026-01-10 00:21:17 +0530

    Basic backend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+    res.send("API Endpoint");
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
 });

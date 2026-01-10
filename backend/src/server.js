b72ae81b7bbd059eacdb7653f559d0ecdc958667
Kshitij <kshitij24.srinivasan@gmail.com>
2026-01-10 10:52:01 +0530

    Connected to MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import { connectDB } from './config/db';
 
 const app = express();
 const PORT = process.env.PORT;
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("API Endpoint");
